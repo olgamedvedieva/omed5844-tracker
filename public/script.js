72a9c1f7078b2dbb5321e0c49b7e491abce447a5
Olga Medvedieva <olga.medvedieva@gmail.com>
2023-06-02 16:27:14 +1000

    added close button to modal

--- public/script.js
+++ public/script.js
@@ -1,5 +1,9 @@
 const modal = document.querySelector("[data-modal]");
 const openButton = document.querySelector(".hero-button");
+const closeButton = document.querySelector(".close-modal-button");
 openButton.addEventListener("click", () => {
   modal.showModal()
+})
+closeButton.addEventListener("click", () => {
+  modal.close()
 })
