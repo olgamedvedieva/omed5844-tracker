087d5506d42079980b95ce22fe7053024a928e00
Olga Medvedieva <olga.medvedieva@gmail.com>
2023-06-04 17:50:15 +1000

    adjusted background scrolling for modal and blur

--- public/script.js
+++ public/script.js
@@ -1,9 +1,14 @@
 const modal = document.querySelector("[data-modal]");
 const openButton = document.querySelector(".hero-button");
 const closeButton = document.querySelector(".close-modal-button");
+const overlay = document.querySelector('.dialog-overlay');
 openButton.addEventListener("click", () => {
   modal.showModal()
-})
+  overlay.classList.add('dialog-open')
+  document.body.style.overflow = 'hidden';
+});
 closeButton.addEventListener("click", () => {
   modal.close()
-})
+  overlay.classList.remove('dialog-open');
+  document.body.style.removeProperty('overflow');
+});
